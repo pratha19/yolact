1b2ad815852f7e77adbac0eac018ec3199c029ef
Prathamesh Pawar <38340534+pratha19@users.noreply.github.com>
2020-08-05 00:50:51 -0700

    adding yolact_resnet50_maps_config

--- data/config.py
+++ data/config.py
@@ -774,6 +774,16 @@ yolact_resnet50_pascal_config = yolact_resnet50_config.copy({
     })
 })
 
+yolact_resnet50_maps_config = yolact_resnet50_config.copy({
+    'name': 'yolact_plus_resnet50_maps',
+    # Dataset stuff
+    'dataset': maps_dataset,
+    'num_classes': len(maps_dataset.class_names) + 1,
+
+    # Image Size
+    'max_size': 512,
+})
+
 # ----------------------- YOLACT++ CONFIGS ----------------------- #
 
 yolact_plus_base_config = yolact_base_config.copy({
